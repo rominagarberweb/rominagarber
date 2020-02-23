9ead8695d52bd48d681ee34fe4808a3ecde5267d
p10e <627189+p10e@users.noreply.github.com>
2020-02-23 20:47:41 +0100

    Added desk structure

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -1,8 +1,18 @@
 import S from '@sanity/desk-tool/structure-builder'
 import {
+  FaRegNewspaper
+} from 'react-icons/fa'
+import {
+  FiBook,
+  FiFeather,
+  FiFileText,
+  FiGrid,
+  FiSettings,
+  FiUser,
+} from 'react-icons/fi'
+import {
   MdEvent,
-  MdPerson,
-  MdSettings
+  MdLightbulbOutline
 } from 'react-icons/md'
 
 const hiddenDocTypes = listItem =>
@@ -14,30 +24,53 @@ export default () =>
     .items([
       S.listItem()
         .title('Blog posts')
+        .icon(FiFeather)
         .schemaType('post')
         .child(S.documentTypeList('post').title('Blog posts')),
+      S.listItem()
+        .title('Books')
+        .icon(FiBook)
+        .schemaType('event')
+        .child(S.documentTypeList('event').title('Events')),
       S.listItem()
         .title('Events')
         .icon(MdEvent)
         .schemaType('event')
         .child(S.documentTypeList('event').title('Events')),
       S.listItem()
-        .title('Authors')
-        .icon(MdPerson)
-        .schemaType('author')
-        .child(S.documentTypeList('author').title('Authors')),
-      S.listItem()
-        .title('Categories')
+        .title('Pages')
+        .icon(FiFileText)
         .schemaType('category')
         .child(S.documentTypeList('category').title('Categories')),
       S.listItem()
+        .title('Press items')
+        .icon(FaRegNewspaper)
+        .schemaType('post')
+        .child(S.documentTypeList('post').title('Blog posts')),
+      S.listItem()
+        .title('Tips')
+        .icon(MdLightbulbOutline)
+        .schemaType('category')
+        .child(S.documentTypeList('category').title('Categories')),
+      S.divider(),
+      S.listItem()
         .title('Settings')
-        .icon(MdSettings)
+        .icon(FiSettings)
         .child(
-          S.editor()
-            .id('siteSettings')
-            .schemaType('siteSettings')
-            .documentId('siteSettings')
+          S.list()
+            .title('Settings')
+            .items([
+              S.listItem()
+                .title('Authors')
+                .icon(FiUser)
+                .schemaType('author')
+                .child(S.documentTypeList('author').title('Authors')),
+              S.listItem()
+                .title('Categories')
+                .icon(FiGrid)
+                .schemaType('category')
+                .child(S.documentTypeList('category').title('Categories')),
+            ])
         ),
       // This returns an array of all the document types
       // defined in schema.js. We filter out those that we have
