59167851ab257a7852951863b4a941ccb9df6a73
p10e <627189+p10e@users.noreply.github.com>
2020-03-28 12:38:32 +0100

    add about page data

---
+++ web/_data/aboutPage.js
@@ -0,0 +1,50 @@
+const BlocksToMarkdown = require('@sanity/block-content-to-markdown')
+const groq = require('groq')
+const client = require('../utils/sanityClient.js')
+const serializers = require('../utils/serializers')
+const overlayDrafts = require('../utils/overlayDrafts')
+const imageUrl = require('../utils/imageUrl')
+const hasToken = !!client.config().token
+
+function generateAboutPage (aboutPage) {
+  return {
+    ...aboutPage,
+    bio: BlocksToMarkdown(
+      aboutPage.bio,
+      {serializers, ...client.config()}
+    ),
+    image: imageUrl(aboutPage.mainImage)
+      .height(580)
+      .width(460)
+      .url(),
+  }
+}
+
+async function getAboutPage () {
+  const filter = groq`*[_type == "aboutPage"]`
+  const projection = groq`{
+    _id,
+    agent{
+      title,
+      url
+    },
+    bio[]{
+      ...,
+      children[]{
+        ...
+      }
+    },
+    mainImage,
+    publishers[]{
+      title,
+      url
+    }
+  }`
+  const query = [filter, projection].join(' ')
+  const docs = await client.fetch(query).catch(err => console.error(err))
+  const reducedDocs = overlayDrafts(hasToken, docs)
+  const prepareAboutPage = reducedDocs.map(generateAboutPage)
+  return prepareAboutPage[0]
+}
+
+module.exports = getAboutPage
