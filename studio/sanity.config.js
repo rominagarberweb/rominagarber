ba13ca5940ae47b64a6543bb62a849e02b7a7a89
Smart AI <63435239+coolidev@users.noreply.github.com>
2023-03-29 18:56:06 -0700

    Revert "studio dashboard"

--- studio/sanity.config.js
+++ studio/sanity.config.js
@@ -3,11 +3,6 @@ import {deskTool} from 'sanity/desk'
 import schemas from './schemas/schema'
 import deskStructure from './deskStructure'
 import {colorInput} from '@sanity/color-input'
-import {
-  dashboardTool,
-  sanityTutorialsWidget
-} from '@sanity/dashboard'
-import { netlifyWidget } from "sanity-plugin-dashboard-widget-netlify";
 
 export default defineConfig({
   title: 'rominagarbar',
@@ -17,30 +12,7 @@ export default defineConfig({
     deskTool({
       structure: deskStructure
     }),
-    colorInput(),
-    dashboardTool({
-      widgets: [
-        netlifyWidget({
-          title: 'Rominagarber deploys',
-          sites: [
-            {
-              title: 'Rominagarber Studio',
-              apiId: 'ea96b6d7-07e2-4399-b12e-bfbdbac7e184',
-              buildHookId: '5e42d092e1fc53d6daed66dc',
-              name: 'rominagarber-studio',
-              url: 'https://rominagarber-studio.netlify.app'
-            },
-            {
-              title: 'Website',
-              apiId: '888eddbc-bff0-4896-a29b-409fbf35dea8',
-              buildHookId: '6424e735fa67466e2403b344',
-              name: 'rominagarber',
-              url: 'https://rominagarber.netlify.app',
-            },
-          ]
-        })
-      ]
-    })
+    colorInput()
   ],
   schema: {
     types: schemas
