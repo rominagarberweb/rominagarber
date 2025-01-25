569d3e6372fa7fa0de856fe1de6fc30894f5ad1f
andypbrowne <andypbrowne@gmail.com>
2025-01-25 18:25:49 -0500

    try to fix the RSS feed

---
+++ web/eleventy.config.js
@@ -0,0 +1,22 @@
+const { feedPlugin } = require("@11ty/eleventy-plugin-rss");
+
+module.exports = function (eleventyConfig) {
+	eleventyConfig.addPlugin(feedPlugin, {
+		type: "rss", // "atom", or "rss", "json"
+		outputPath: "/feed.xml",
+		collection: {
+			name: "posts", // iterate over `collections.posts`
+			limit: 0,     // 0 means no limit
+		},
+		metadata: {
+			language: "en",
+			title: "Romina's Blog",
+			subtitle: "About Young Adult books and writing tips.",
+			base: "https://rominagarber.com/",
+			author: {
+				name: "Romina Garber",
+				email: "", // Optional
+			}
+		}
+	});
+};
