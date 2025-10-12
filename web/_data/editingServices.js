36da7e332f658a15dc8e666d9585feb26ea7094b
andypbrowne <andypbrowne@gmail.com>
2025-10-12 12:40:26 -0400

    start creating the editing services web template

---
+++ web/_data/editingServices.js
@@ -0,0 +1,86 @@
+const BlocksToMarkdown = require('@sanity/block-content-to-markdown')
+const groq = require('groq')
+const client = require('../utils/sanityClient.js')
+const serializers = require('../utils/serializers')
+const overlayDrafts = require('../utils/overlayDrafts')
+const imageUrl = require('../utils/imageUrl')
+const hasToken = !!client.config().token
+
+function extractYouTubeId (url = '') {
+  if (!url) return null
+  let id = null
+  try {
+    const u = new URL(url)
+    if (u.hostname.includes('youtube.com')) {
+      id = u.searchParams.get('v')
+    } else if (u.hostname.includes('youtu.be')) {
+      id = u.pathname.replace('/', '')
+    }
+  } catch (e) {
+    // fallback parsing
+    const vMatch = url.match(/v=([^&]+)/)
+    if (vMatch) id = vMatch[1]
+    else {
+      const short = url.split('/').pop()
+      id = short || null
+    }
+  }
+  if (!id) return null
+  const amp = id.indexOf('&')
+  return amp === -1 ? id : id.substring(0, amp)
+}
+
+function prepareServiceCard (card = {}) {
+  return {
+    ...card,
+    description: card.description ? BlocksToMarkdown(card.description, { serializers, ...client.config() }) : null,
+    image: card.image ? imageUrl(card.image).height(260).width(160).url() : null
+  }
+}
+
+function generateEditingServices (doc = {}) {
+  const youtubeId = doc.youtube && doc.youtube.url ? extractYouTubeId(doc.youtube.url) : null
+  return {
+    ...doc,
+    title: doc.title || null,
+    description: doc.description || null,
+    image: doc.mainImage ? imageUrl(doc.mainImage).height(580).width(460).url() : null,
+    youtube: doc.youtube
+      ? {
+          url: doc.youtube.url || null,
+          description: doc.youtube.description || null,
+          preferVideo: !!doc.youtube.preferVideo,
+          id: youtubeId,
+          thumbnail: youtubeId ? `https://img.youtube.com/vi/${youtubeId}/hqdefault.jpg` : null
+        }
+      : null,
+    introduction: doc.introduction ? BlocksToMarkdown(doc.introduction, { serializers, ...client.config() }) : null,
+    serviceCards: Array.isArray(doc.serviceCards) ? doc.serviceCards.map(prepareServiceCard) : []
+  }
+}
+
+async function getEditingServices () {
+  const filter = groq`*[_type == "editingServices"]`
+  const projection = groq`{
+    _id,
+    title,
+    description,
+    mainImage,
+    youtube{ url, description, preferVideo },
+    introduction[]{
+      ...,
+      children[]{ ... }
+    },
+    serviceCards[]{
+      ...,
+      image
+    }
+  }`
+  const query = [filter, projection].join(' ')
+  const docs = await client.fetch(query).catch(err => { console.error('editingServices data fetch error', err); return [] })
+  const reduced = overlayDrafts(hasToken, docs)
+  const prepared = reduced.map(generateEditingServices)
+  return prepared[0] || null
+}
+
+module.exports = getEditingServices
