652d872dc5bb73978af292f540a1f7b88701c7c3
andypbrowne <andypbrowne@gmail.com>
2026-02-17 15:25:21 -0500

    Fix: add no-cookies parameter to Sanity CDN image URLs

--- web/utils/imageUrl.js
+++ web/utils/imageUrl.js
@@ -2,18 +2,38 @@ const SanityImageUrl = require('@sanity/image-url')
 const sanityClient = require('./sanityClient')
 
 // Learn more: https://www.sanity.io/docs/asset-pipeline/image-urls
+// Prevents third-party cookies by appending no-cookies parameter
 function urlFor(source) {
   const builder = SanityImageUrl(sanityClient).image(source)
   
-  // Wrap the url() method to append no-cookies parameter
-  const originalUrl = builder.url.bind(builder)
-  builder.url = function() {
-    const url = originalUrl()
-    const separator = url.includes('?') ? '&' : '?'
-    return `${url}${separator}no-cookies=true`
+  // Create a wrapper object that intercepts method calls
+  const wrappedBuilder = {
+    // Store original builder methods for chaining
+    _builder: builder,
+    
+    height(h) {
+      this._builder = this._builder.height(h)
+      return this
+    },
+    
+    width(w) {
+      this._builder = this._builder.width(w)
+      return this
+    },
+    
+    rect(left, top, width, height) {
+      this._builder = this._builder.rect(left, top, width, height)
+      return this
+    },
+    
+    url() {
+      const url = this._builder.url()
+      const separator = url.includes('?') ? '&' : '?'
+      return `${url}${separator}no-cookies=true`
+    }
   }
   
-  return builder
+  return wrappedBuilder
 }
 
 module.exports = urlFor
