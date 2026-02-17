a9ffa96cc3cc38ff97ed99c76da649156e395828
andypbrowne <andypbrowne@gmail.com>
2026-02-17 14:52:08 -0500

    no cookies on sanity images

--- web/utils/imageUrl.js
+++ web/utils/imageUrl.js
@@ -1,9 +1,19 @@
-const imageUrl = require('@sanity/image-url')
+const SanityImageUrl = require('@sanity/image-url')
 const sanityClient = require('./sanityClient')
 
 // Learn more: https://www.sanity.io/docs/asset-pipeline/image-urls
 function urlFor(source) {
-  return imageUrl(sanityClient).image(source)
+  const builder = SanityImageUrl(sanityClient).image(source)
+  
+  // Wrap the url() method to append no-cookies parameter
+  const originalUrl = builder.url.bind(builder)
+  builder.url = function() {
+    const url = originalUrl()
+    const separator = url.includes('?') ? '&' : '?'
+    return `${url}${separator}no-cookies=true`
+  }
+  
+  return builder
 }
 
 module.exports = urlFor
