7472f6cf3cf1b850bf9d3a813d2e4d41de4966d6
andypbrowne <andypbrowne@gmail.com>
2026-08-28 16:13:01 -0400

    Refactor bodyPortableText schema to use JSX

---
+++ studio/schemas/objects/bodyPortableText.jsx
@@ -0,0 +1,134 @@
+import React from 'react'
+import {FiAlignCenter, FiAlignLeft, FiAlignRight} from 'react-icons/fi'
+
+function AlignLeft({children}) {
+  return <span style={{display: 'block', textAlign: 'left'}}>{children}</span>
+}
+
+function AlignCenter({children}) {
+  return <span style={{display: 'block', textAlign: 'center'}}>{children}</span>
+}
+
+function AlignRight({children}) {
+  return <span style={{display: 'block', textAlign: 'right'}}>{children}</span>
+}
+
+function Lowercase({children}) {
+  return <span style={{textTransform: 'lowercase'}}>{children}</span>
+}
+
+function Uppercase({children}) {
+  return <span style={{textTransform: 'uppercase'}}>{children}</span>
+}
+
+function Capitalize({children}) {
+  return <span style={{textTransform: 'capitalize'}}>{children}</span>
+}
+
+function SmallCaps({children}) {
+  return <span style={{fontVariantCaps: 'small-caps'}}>{children}</span>
+}
+
+function AllSmallCaps({children}) {
+  return <span style={{fontVariantCaps: 'all-small-caps'}}>{children}</span>
+}
+
+function CaseIcon({label, style}) {
+  return <span style={{fontSize: '0.75em', fontWeight: 600, ...style}}>{label}</span>
+}
+
+export default {
+  name: 'bodyPortableText',
+  type: 'array',
+  title: 'Post body',
+  of: [
+    {
+      type: 'block',
+      title: 'Block',
+      styles: [
+        {title: 'Normal', value: 'normal'},
+        {title: 'H2', value: 'h2'},
+        {title: 'H3', value: 'h3'},
+        {title: 'H4', value: 'h4'},
+        {title: 'Quote', value: 'blockquote'}
+      ],
+      lists: [
+        {title: 'Bullet', value: 'bullet'},
+        {title: 'Number', value: 'number'}
+      ],
+      marks: {
+        decorators: [
+          {title: 'Strong', value: 'strong'},
+          {title: 'Emphasis', value: 'em'},
+          {
+            title: 'Align left',
+            value: 'alignLeft',
+            icon: FiAlignLeft,
+            component: AlignLeft
+          },
+          {
+            title: 'Align center',
+            value: 'alignCenter',
+            icon: FiAlignCenter,
+            component: AlignCenter
+          },
+          {
+            title: 'Align right',
+            value: 'alignRight',
+            icon: FiAlignRight,
+            component: AlignRight
+          },
+          {
+            title: 'Lowercase',
+            value: 'lowercase',
+            icon: () => <CaseIcon label="aa" />,
+            component: Lowercase
+          },
+          {
+            title: 'Uppercase',
+            value: 'uppercase',
+            icon: () => <CaseIcon label="AA" />,
+            component: Uppercase
+          },
+          {
+            title: 'Capitalize',
+            value: 'capitalize',
+            icon: () => <CaseIcon label="Aa" />,
+            component: Capitalize
+          },
+          {
+            title: 'Small caps',
+            value: 'smallCaps',
+            icon: () => <CaseIcon label="Sc" style={{fontVariantCaps: 'small-caps'}} />,
+            component: SmallCaps
+          },
+          {
+            title: 'All small caps',
+            value: 'allSmallCaps',
+            icon: () => <CaseIcon label="SC" style={{fontVariantCaps: 'all-small-caps'}} />,
+            component: AllSmallCaps
+          }
+        ],
+        annotations: [
+          {
+            name: 'link',
+            type: 'object',
+            title: 'URL',
+            fields: [
+              {
+                title: 'URL',
+                name: 'href',
+                type: 'url'
+              }
+            ]
+          }
+        ]
+      },
+      of: [{type: 'tipReference'}]
+    },
+    {
+      type: 'mainImage',
+      options: {hotspot: true}
+    }
+  ]
+}
--- web/utils/serializers.js
+++ web/utils/serializers.js
@@ -1,4 +1,12 @@
+const markdownIt = require('markdown-it')
 const imageUrl = require('./imageUrl')
+
+const inlineMd = markdownIt({html: true, breaks: false, linkify: false})
+
+function classMark (className, props) {
+  const content = Array.isArray(props.children) ? props.children.join('') : (props.children || '')
+  return `<span class="${className}">${inlineMd.renderInline(String(content))}</span>`
+}
 
 // Learn more on https://www.sanity.io/docs/guides/introduction-to-portable-text
 module.exports = {
@@ -7,5 +15,15 @@ module.exports = {
     code: ({node}) =>
       '```' + node.language + '\n' + node.code + '\n```',
     mainImage: ({node}) => `![${node.alt}](${imageUrl(node).width(600).url()})`
+  },
+  marks: {
+    alignLeft: props => classMark('pt-align-left', props),
+    alignCenter: props => classMark('pt-align-center', props),
+    alignRight: props => classMark('pt-align-right', props),
+    lowercase: props => classMark('pt-lowercase', props),
+    uppercase: props => classMark('pt-uppercase', props),
+    capitalize: props => classMark('pt-capitalize', props),
+    smallCaps: props => classMark('pt-small-caps', props),
+    allSmallCaps: props => classMark('pt-all-small-caps', props)
   }
 }
